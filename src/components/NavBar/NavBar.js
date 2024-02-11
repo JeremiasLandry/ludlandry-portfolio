dd87b936befa8971f9515b561b0e5f98e459a44b
Ludmila Landry <ludmilajlandry@gmail.com>
2024-02-10 22:08:44 -0300

    responsive navbar added

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,235 +1,138 @@
-import React, {useState, useEffect} from 'react'
-import { Link } from 'react-router-dom'
-import { Navbar, Container, Nav } from 'react-bootstrap';
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import '../../App.css';
+import menuIcon from './hamburger.png';
+
+const MenuHamburguesa = () => {
+  const [menuAbierto, setMenuAbierto] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuAbierto(!menuAbierto);
+  };
+
+  return (
+    <div className='menu-ctn'>
+      {/* Aquí va el icono que funcionará como el botón del menú */}
+      <img src={menuIcon} alt="Icono Menú" className='hamburger-icon' onClick={toggleMenu} />
+
+      {/* Aquí va el contenido del menú que se mostrará o ocultará */}
+      {menuAbierto && (
+        <div className="menu">
+          {/* Contenido del menú */}
+          <ul>
+              <li className='responsive-link-nav'>
+                <Link to="#AboutMe">Sobre mí</Link>
+              </li>
+      
+              <li className='responsive-link-nav'>
+                <Link to="#Skills">Habilidades</Link>
+              </li>
+            
+              <li className='responsive-link-nav'>
+                <Link to="#Projects">Proyectos</Link>
+              </li>
+              <li className='responsive-link-nav'>
+                <Link to="#Projects">Contacto</Link>
+              </li>
+          </ul>
+        </div>
+      )}
+    </div>
+  );
+};
 
 const NavBar = () => {
   const [pantalla, setPantalla] = useState('');
 
-  const manejarCambioEnPantalla = (mediaQueryList) => {
-    if (mediaQueryList[0].matches) {
-      setPantalla('xl');
-    } else if (mediaQueryList[1].matches) {
-      setPantalla('lg');
-    } else if (mediaQueryList[2].matches) {
-      setPantalla('md');
-    } else if (mediaQueryList[3].matches) {
-      setPantalla('sm');
-    } else if (mediaQueryList[4].matches) {
-      setPantalla('xs');
-    }
-  };
-
   useEffect(() => {
     const mediaQueries = [
-      window.matchMedia('(min-width: 1280px)'),
-      window.matchMedia('(max-width: 1279px)'),
-      window.matchMedia('(max-width: 1050px)'),
+      window.matchMedia('(max-width: 700px)'),
       window.matchMedia('(max-width: 800px)'),
-      window.matchMedia('(max-width: 400px)'),
+      window.matchMedia('(max-width: 1050px)'),
+      window.matchMedia('(max-width: 1279px)'),
+      window.matchMedia('(min-width: 1280px)'),
     ];
 
-    manejarCambioEnPantalla(mediaQueries.map(query => query));
+    const manejarCambioEnPantalla = () => {
+      const tamanios = ['xs', 'sm', 'md', 'lg', 'xl'];
+      const indice = mediaQueries.findIndex(query => query.matches);
+      setPantalla(tamanios[indice]);
+    };
 
-    mediaQueries.forEach((mediaQuery, index) => {
-      mediaQuery.addListener(() => {
-        manejarCambioEnPantalla(mediaQueries.map(query => query));
-      });
+    mediaQueries.forEach(mediaQuery => {
+      mediaQuery.addListener(manejarCambioEnPantalla);
     });
 
+    // Llama a la función para manejar el estado inicial
+    manejarCambioEnPantalla();
+
+    // Limpia los event listeners al desmontar el componente
     return () => {
       mediaQueries.forEach(mediaQuery => {
-        mediaQuery.removeListener(() => {
-          manejarCambioEnPantalla(mediaQueries.map(query => query));
-        });
+        mediaQuery.removeListener(manejarCambioEnPantalla);
       });
     };
   }, []);
 
-  
+  const navClasses = {
+    xs: 'nav-xs',
+    sm: 'nav-sm',
+    md: 'nav-md',
+    lg: 'nav-lg',
+    xl: '',
+  };
 
   return (
-    <>
-      {pantalla === 'xl' && (
-        <nav>
-          <ul>
-            <div className='nombre'>
-              <li>
-                  <h1><strong>Ludmila Landry</strong></h1>
-              </li>
-            </div>
+    <nav className={navClasses[pantalla]}>
+      <ul>
+        <div className='nombre'>
+          <li>
+            <h1><strong>Ludmila Landry</strong></h1>
+          </li>
+        </div>
+        <div>
+          <hr />
+        </div>
+        {pantalla === 'xs' ? (
+          <MenuHamburguesa />
+        ) : (
+          <div className='navLinksGroup'>
+            <li>
+              <Link to="#AboutMe">Sobre mí</Link>
+            </li>
+            <span>|</span>
+            <li>
+              <Link to="#Skills">Habilidades</Link>
+            </li>
+            <span>|</span>
+            <li>
+              <Link to="#Projects">Proyectos</Link>
+            </li>
+            {pantalla === 'md' || pantalla === 'xs' || pantalla === 'sm' ? (
+              <>
+                <span>|</span>
+                <li>
+                  <Link to="/contact">Contacto</Link>
+                </li>
+              </>
+            ) : ''}
+          </div>
+        )}
+        {pantalla === 'md' || pantalla === 'xs' || pantalla === 'sm' ? '' : (
+          <>
             <div>
-              <hr/>
-            </div>
-            <div>
-              <li>
-                  <Link to="#AboutMe">Sobre mí</Link>
-              </li>
-              <span>|</span>
-              <li>
-                  <Link to="#Skills">Habilidades</Link>
-              </li>
-              <span>|</span>
-              <li>
-                  <Link to="#Projects">Proyectos</Link>
-              </li>
-            </div>
-            <div>
-              <hr className='contact-line'/>
+              <hr className='contact-line' />
             </div>
             <div className='navContact'>
               <li>
                 <Link to="/contact" className='contact-button'>Contacto</Link>
               </li>
-            </div> 
-          </ul>
-        </nav>
-      )}
-      {/* pantalla Large 800px */}
-      {pantalla === 'lg' && (
-         <nav className='nav-lg'>
-          <ul>
-            <div className='nombre'>
-              <li>
-                  <h1><strong>Ludmila Landry</strong></h1>
-              </li>
             </div>
-            <div>
-              <hr/>
-            </div>
-            <div>
-              <li>
-                  <Link to="#AboutMe">Sobre mí</Link>
-              </li>
-              <span>|</span>
-              <li>
-                  <Link to="#Skills">Habilidades</Link>
-              </li>
-              <span>|</span>
-              <li>
-                  <Link to="#Projects">Proyectos</Link>
-              </li>
-            </div>
-            <div>
-              <hr className='contact-line'/>
-            </div>
-            <div className='navContact'>
-              <li>
-                <Link to="/contact" className='contact-button'>Contacto</Link>
-              </li>
-            </div> 
-          </ul>
-       </nav>
-      )}
-      {pantalla === 'md' && (
-         <nav className='nav-md'>
-          <ul>
-            <div className='nombre'>
-              <li>
-                  <h1><strong>Ludmila Landry</strong></h1>
-              </li>
-            </div>
-            <div>
-              <hr/>
-            </div>
-            <div>
-              <li>
-                  <Link to="#AboutMe">Sobre mí</Link>
-              </li>
-              <span>|</span>
-              <li>
-                  <Link to="#Skills">Habilidades</Link>
-              </li>
-              <span>|</span>
-              <li>
-                  <Link to="#Projects">Proyectos</Link>
-              </li>
-            </div>
-            <div>
-              <hr className='contact-line'/>
-            </div>
-            <div className='navContact'>
-              <li>
-                <Link to="/contact" className='contact-button'>Contacto</Link>
-              </li>
-            </div> 
-          </ul>
-       </nav>
-      )}
-      {pantalla === 'sm' && (
-         <nav className='nav-sm'>
-          <ul>
-            <div className='nombre'>
-              <li>
-                  <h1><strong>Ludmila Landry</strong></h1>
-              </li>
-            </div>
-            <div>
-              <hr/>
-            </div>
-            <div>
-              <li>
-                  <Link to="#AboutMe">Sobre mí</Link>
-              </li>
-              <span>|</span>
-              <li>
-                  <Link to="#Skills">Habilidades</Link>
-              </li>
-              <span>|</span>
-              <li>
-                  <Link to="#Projects">Proyectos</Link>
-              </li>
-            </div>
-            <div>
-              <hr className='contact-line'/>
-            </div>
-            <div className='navContact'>
-              <li>
-                <Link to="/contact" className='contact-button'>Contacto</Link>
-              </li>
-            </div> 
-          </ul>
-       </nav>
-      )}
-      {pantalla === 'xs' && (
-         <nav className='nav-xs'>
-          <ul>
-            <div className='nombre'>
-              <li>
-                  <h1><strong>Ludmila Landry</strong></h1>
-              </li>
-            </div>
-            <div>
-              <hr/>
-            </div>
-            <div>
-              <li>
-                  <Link to="#AboutMe">Sobre mí</Link>
-              </li>
-              <span>|</span>
-              <li>
-                  <Link to="#Skills">Habilidades</Link>
-              </li>
-              <span>|</span>
-              <li>
-                  <Link to="#Projects">Proyectos</Link>
-              </li>
-            </div>
-            <div>
-              <hr className='contact-line'/>
-            </div>
-            <div className='navContact'>
-              <li>
-                <Link to="/contact" className='contact-button'>Contacto</Link>
-              </li>
-            </div> 
-          </ul>
-       </nav>
-      )}
-
-
-    </>
-    
-  )
-}
+          </>
+        )}
+      </ul>
+    </nav>
+  );
+};
 
 export default NavBar;
